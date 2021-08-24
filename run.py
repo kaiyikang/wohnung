c4c2726b9e4992d18dee5e489bbe738e5c8f2a12
kaiyikang <kaiyikang@gmail.com>
2021-08-24 16:08:01 +0200

    create latest time

--- run.py
+++ run.py
@@ -3,3 +3,4 @@ import datetime
 time = datetime.datetime.now().strftime('%Y_%m_%d')
 
 cmdline.execute(['scrapy', 'crawl','price','-O','dataset/'+time+'.json',])
+cmdline.execute(['scrapy', 'crawl','price','-O','dataset/'+'latest.json',])
